57b81e4835eb9297ff6348ebde2e9400c2181d0d
huy.luong <raymondhuy177@gmail.com>
2017-06-21 15:55:43 +0700

    add PrivacyABAC.Functions/UserDefinedFunctionFactory.cs

---
+++ PrivacyABAC.Functions/UserDefinedFunctionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrivacyABAC.Functions
+{
+    public class UserDefinedFunctionFactory
+    {
+        private UserDefinedFunctionFactory() { }
+
+        private static UserDefinedFunctionFactory _instance;
+
+        public static UserDefinedFunctionFactory GetInstance()
+        {
+            return _instance;
+        }
+
+        public void RegisterFunction(IPluginFunction function)
+        {
+        }
+    }
+}
