113bfa1c491ca2cf3258306b526a306a9dd372a4
51301464@hcmut.edu.vn <51301464@hcmut.edu.vn>
2017-06-24 16:03:31 +0700

    add model to dbInterfaces

---
+++ PrivacyABAC.DbInterfaces/Model/AccessControlPolicyCombining.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrivacyABAC.DbInterfaces.Model
+{
+    public class AccessControlPolicyCombining
+    {
+        
+        public ICollection<string> PolicyIds { get; set; }
+        
+        public string Algorithm { get; set; }
+    }
+}
